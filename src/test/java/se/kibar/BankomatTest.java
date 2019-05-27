b51318c5b91517edf216f0438b088ee17b43eeff
zgrkbr <ozgurkibar78@gmail.com>
2019-05-27 10:42:40 +0200

    testing the actual method Bankomat.login()

--- src/test/java/se/kibar/BankomatTest.java
+++ src/test/java/se/kibar/BankomatTest.java
@@ -8,12 +8,16 @@ public class BankomatTest {
 
     @Test
     public void loginTest() {
+        Bankomat testBankomat = new Bankomat();
+
         Account testAccount = new Account(2000.0, "1234");
         String testPinCode1 = "1234";
         String testPinCode2 = "0582";
 
-        assertTrue(testAccount.getPinCode().equals(testPinCode1));
-        assertFalse(testAccount.getPinCode().equals(testPinCode2));
+//        assertTrue(testAccount.getPinCode().equals(testPinCode1));
+//        assertFalse(testAccount.getPinCode().equals(testPinCode2));
 
+        assertTrue(testBankomat.login(testAccount, testPinCode1));
+        
     }
 }
